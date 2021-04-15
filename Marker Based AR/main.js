7fa06c97d0be34c25d82b702f7cfeecadba25928
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2021-04-15 17:44:34 -0400

    added the images

--- Marker Based AR/main.js
+++
@@ -1,7 +0,0 @@
-let today = new Date()
-
-let time = today.getHours()+ ":" + today.getMinutes()
-time.toString()
-document.querySelector("currentTime").value = time
-
-document.getElementsByTagName("")
